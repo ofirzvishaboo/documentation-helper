e16dea7667b49d7a8f5bf9ef8391a6c0aca5fe07
ofirzvishaboo <ofirfirst@gmail.com>
2025-11-09 01:43:15 +0200

    Added dependencies, import and env_vars

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 import asyncio
+import os
+import ssl
+from typing import Any, Dict, List
+
+import certifi
+import dotenv
+from langchain_tavily import TavilyCrawl, TavilyMap, TavilyExtract
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from langchain_pinecone import PineconeVectorStore
+from logger import (Colors, log_info, log_success, log_error, log_warning, log_header)
+
+dotenv.load_dotenv()
+
+
+ssl.create_default_context(cafile=certifi.where())
+os.environ["SSL_CERT_FILE"] = certifi.where()
+os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-small", show_progress_bar=False, chunk_size=50, retry_min_seconds=10
+)
+
+# chroma = Chroma(persistence_directory="chroma_db", embedding_function=embeddings)
+vectorstore = PineconeVectorStore(index_name="langchain-doc-index", embedding=embeddings)
+tavily_extract = TavilyExtract()
+tavily_map = TavilyMap(max_depth=5, max_breadth=20, max_pages=1000)
+tavily_crawl = TavilyCrawl()
+
 
 async def main():
     print("Hello from documentation-helper!")
-    
+
 
 if __name__ == "__main__":
     asyncio.run(main())
