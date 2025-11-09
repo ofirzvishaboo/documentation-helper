d492f85ccac6bcd066642ab1db0b690af622de6e
ofirzvishaboo <ofirfirst@gmail.com>
2025-11-09 22:10:05 +0200

    initializing tavily crawl and loading docs

--- ingestion.py
+++ ingestion.py
@@ -32,7 +32,26 @@ tavily_crawl = TavilyCrawl()
 
 
 async def main():
-    print("Hello from documentation-helper!")
+    """Main async function to the entire program."""
+    log_header("Documentation Ingestion Pipeline")
+
+    log_info(
+        "Tavily Crawl: Starting to crawl documentation from https://python.langchain.com",
+        Colors.PURPLE,
+    )
+
+    res = tavily_crawl.invoke({
+        "url": "https://python.langchain.com",
+        "max_depth": 5,
+        "extract_depth": "advanced",
+        "instructions": "content on ai agents",
+    })
+
+    all_docs = [Document(page_content=result["raw_content"], metadata={"source": result["url"]}) for result in res["results"]]
+
+    log_success(
+        f"Tavily Crawl: Successfully crawled {len(all_docs)} urls from documentation site",
+    )
 
 
 if __name__ == "__main__":
